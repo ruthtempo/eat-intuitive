8c104c5d10c572e1c4491b38448f1f082569cdb4
Ruth <ruthtemp@gmail.com>
2022-05-23 17:40:22 +0200

    dynamic calendar using date-fns

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -3,9 +3,13 @@ import { Button, Table } from 'react-bootstrap'
 import './Calendar.css'
 import { CaretLeft, CaretRight } from "react-bootstrap-icons";
 import getDaysInMonth from "date-fns/getDaysInMonth";
+import { getWeeksInMonth } from "date-fns";
+import { add, startOfMonth, sub } from "date-fns/esm";
+import format from "date-fns/format";
 
-const date = new Date(2022, 1)
-const numberOfDays = getDaysInMonth(date) //number
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 interface CalendarRecord {
   day: number,
@@ -15,10 +19,15 @@ interface CalendarRecord {
 
 
 function Calendario() {
+
   const weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const daysInMonth: CalendarRecord[] = []
-  let weeksInMonth: number[] = []
+  // const [currentMonth, setCurrentMonth] = useState(today.getMonth())
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const numberOfDays = getDaysInMonth(currentDate) //number
+  const numberOfWeeks = getWeeksInMonth(currentDate)
+  const monthBeginning = startOfMonth(currentDate)
+
 
   function getDaysArray(n: number) {
     for (let i = 1; i <= n; i++) {
@@ -27,102 +36,43 @@ function Calendario() {
         checked: false
       })
     }
-    console.log(daysInMonth)
-
   }
+
+
   getDaysArray(numberOfDays)
 
+  function getWeeks(days: CalendarRecord[], weeksCount: number): (CalendarRecord | undefined)[][] {
+    const month: (CalendarRecord | undefined)[][] = []
+    let counter = 0
+    //weeks
+    for (let j = 1; j <= weeksCount; j++) {
+      const week = [] //array will be created 5 times, and will iterate the subloop 7 times every time (5 columns of 7 days each)
 
-  const rows = [[
-    {
-      day: 1,
-      checked: false
-    },
-    {
-      day: 2,
-      checked: true
-    },
-    {
-      day: 3,
-      checked: true
-    },
-    {
-      day: 4,
-      checked: true
-    },
-    {
-      day: 5,
-      checked: true
-    },
-    {
-      day: 6,
-      checked: true
-    },
-    {
-      day: 7,
-      checked: true
-    }], [
-    {
-      day: 8,
-      checked: true
-    },
-    {
-      day: 9,
-      checked: true
-    },
-    {
-      day: 10,
-      checked: false
-    },
-    {
-      day: 11,
-      checked: true
-    },
-    {
-      day: 12,
-      checked: true
-    },
-    {
-      day: 13,
-      checked: true
-    },
-    {
-      day: 14,
-      checked: false
+      //days
+      for (let i = 0; i <= 6; i++) { //days of week
+        week.push(days[counter]) //ich brauche ein index das uber die month days  - it does not exist yet
+        counter++
+      };
+
+      month.push(week)//5 arrays of numbers in total
+
     }
-  ], [
-    {
-      day: 15,
-      checked: true
-    },
-    {
-      day: 16,
-      checked: true
-    },
-    {
-      day: 17,
-      checked: true
-    },
-    {
-      day: 18,
-      checked: false
-    },
-    {
-      day: 19,
-      checked: true
-    },
-    {
-      day: 20,
-      checked: false
-    },
-    {
-      day: 21,
-      checked: true
-    }
+    return month
+  }
 
-  ]
-  ]
 
+  const rows = getWeeks(daysInMonth, numberOfWeeks)
+
+  function nextMonth() {
+    setCurrentDate(add(currentDate, { months: 1 }))
+  }
+
+  function prevMonth() {
+    setCurrentDate(sub(currentDate, { months: 1 }))
+  }
+
+
+  console.log("currentmonth: ", currentDate)
 
 
   return (
@@ -132,9 +82,9 @@ function Calendario() {
           <tr>
             <th colSpan={7}>
               <div className="d-flex justify-content-between">
-                <Button variant="light"><CaretLeft /></Button>
-                MAY 2022
-                <Button variant="light"><CaretRight /></Button>
+                <Button variant="light" onClick={prevMonth}><CaretLeft /></Button>
+                {format(currentDate, 'MMM yyyy')}
+                <Button variant="light" onClick={nextMonth} ><CaretRight /></Button>
               </div>
             </th>
           </tr>
@@ -146,9 +96,9 @@ function Calendario() {
             ))}
           </tr>
           {rows.map(row => (
-            <tr key={row[0].day} >
+            <tr key={row[0]?.day} >
               {row.map(day => (
-                <td key={day.day} className={day.checked ? "bg-success text-white" : " "}>{day.day}</td>
+                <td key={day?.day} className={day?.checked ? "bg-success text-white" : " "}>{day?.day}</td>
               ))}
             </tr>
           ))}
