6f8a963fcdb90d657679a6b19d1b5dc943482fa7
Ruth <ruthtemp@gmail.com>
2022-06-14 11:04:11 +0200

    fix

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { ResourceIntuCheck } from "./ResourceIntuCheck"
 import { ResourceRaisin } from "./ResourceRaisin"
 import { ResourcesNav } from "./ResourcesNav"
--- src/components/ResourcesNav.tsx
+++ src/components/ResourcesNav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 
