9bbf8998499b25a4cce28e91e4a0ea2111e6b8f1
Ruth Temporal <ruthtemp@gmail.com>
2022-10-20 17:48:27 +0200

    fix image width screen phone

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import intueative from '../intueative_lon.png';
-import subtitle from '../untertitel.svg'
+import intueative from "../intueative_lon.png";
+import subtitle from "../untertitel.svg";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react"
-import 'animate.css'
+import { useState } from "react";
+import "animate.css";
 
 export function Home() {
-
   const navigate = useNavigate();
   const navigateToScale = () => {
-    navigate('/scale')
-  }
+    navigate("/scale");
+  };
 
-  const [pulse, setPulse] = useState(0)
-
-
+  const [pulse, setPulse] = useState(0);
 
   return (
     <>
-      <Container className=" my-2 d-flex flex-column justify-content-between text-center">
+      <Container className=" my-2 d-flex flex-column justify-content-between text-center align-items-center">
         <img
           src={intueative}
           alt="logo"
           onClick={() => setPulse(1)}
           onAnimationEnd={() => setPulse(0)}
-          className={pulse ? "animate__animated animate__pulse" : ""} />
+          className={`w-100 ${pulse ? "animate__animated animate__pulse" : ""}`}
+        />
         <img src={subtitle} alt="subtitle" />
-        <Button onClick={navigateToScale} className="bg-primary text-white mt-4">Start</Button>
+        <Button
+          onClick={navigateToScale}
+          className="bg-primary text-white mt-4"
+        >
+          Start
+        </Button>
       </Container>
     </>
-  )
+  );
 }
