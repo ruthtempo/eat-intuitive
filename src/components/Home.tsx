f096baa2e7a6d8482e61aab40edd43e97e72a9f6
Ruth Temporal <ruthtemp@gmail.com>
2022-10-20 18:04:17 +0200

    fix deploy warning

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { Container, Button } from "react-bootstrap";
+import "animate.css";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import intueative from "../intueative_lon.png";
 import subtitle from "../untertitel.svg";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import "animate.css";
 
 export function Home() {
   const navigate = useNavigate();
